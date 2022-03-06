d42cd3ee3e2dd0774e4f5b87a4a190bffaf669fe
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-06 15:08:44 -0300

    react fragment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function App() {
-  return <h1>Functional Component</h1>
+  return (
+    <>
+      <h1>Functional Component</h1>
+      <h2>Caption</h2>
+    </>
+  )
 }
 
 export default App
