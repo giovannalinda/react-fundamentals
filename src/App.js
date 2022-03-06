d450ea2fc57d4da281b1f64beb21d900bfbcf1b1
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-06 15:37:13 -0300

    what are properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
+import Post from './Post'
+
 const category = 'Posts of the week'
 
+// Props(Properties)
 function App() {
   return (
     <>
       <h1>Blog</h1>
       <h3>{category}</h3>
+
+      <hr />
+
+      <Post 
+        post={{
+          title: 'News 01',
+          subtitle: 'Subtitle 01'
+        }}
+      />
+      <Post 
+        post={{
+          title: 'News 02',
+          subtitle: 'Subtitle 02'
+        }}
+      />
+      <Post 
+        post={{
+          title: 'News 03',
+          subtitle: 'Subtitle 03'
+        }}
+      />
     </>
   )
 }
