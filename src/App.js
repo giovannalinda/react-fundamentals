349ad5bc88258130605aa314030b22e09cd2398e
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-06 15:12:44 -0300

    JS expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+const category = 'Posts of the week'
+
 function App() {
   return (
     <>
-      <h1>Functional Component</h1>
-      <h2>Caption</h2>
+      <h1>Blog</h1>
+      <h3>{category}</h3>
     </>
   )
 }
