f916762fc460b200f080e8b238058a32e1fe7ee8
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-07 00:27:15 -0300

    Working with css files

--- src/Header.js
+++ src/Header.js
@@ -10,13 +10,7 @@ export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext)
 
   return (
-    <div 
-      style={{
-        background: '#f2f2f2', 
-        padding: '30px', 
-        fontFamily: 'sans-serif'
-      }}
-    >
+    <div>
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>Change theme</Button>
       {props.children}
