145ebb9f0a265b2f4068ab0ffb0c3d0d84e89b4b
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-18 16:16:38 -0300

    404 Error Page and Switch Component

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 import Posts from './pages/Posts'
 
 export default function Routes() {
   return (
-    <>
+    <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/posts" component={Posts} />
-    </>
+      <Route path="*" component={NotFound} />
+    </Switch>
   )
 }
